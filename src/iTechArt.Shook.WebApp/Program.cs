91a89455c25d8b4c7d08932d4d9d78f504eff69c
Ales Wirt <sanichwi@gmail.com>
2021-12-13 09:23:20 +0300

    Delete comments

--- src/iTechArt.Shook.WebApp/Program.cs
+++ src/iTechArt.Shook.WebApp/Program.cs
@@ -1,4 +1,3 @@
-using iTechArt.Common;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -10,17 +9,6 @@ namespace iTechArt.Shook.WebApp
     {
         public static void Main(string[] args)
         {
-            //var configuration = new ConfigurationBuilder()
-            //    .AddJsonFile("appsettings.json")
-            //    .Build();
-
-            //Logger logger = new Logger(
-            //    new LoggerConfiguration()
-            //    .ReadFrom.Configuration(configuration)
-            //    .CreateLogger());
-
-            //logger.Log(LogLevel.Info, "***Hello, first logger!***");
-
             CreateHostBuilder(args).Build().Run();
         }
 
