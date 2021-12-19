21a9d418ca289388ac62fcc847f8eddc864e9d5e
Ales Wirt <sanichwi@gmail.com>
2021-12-19 16:37:01 +0300

    Create User class, add Identity service into Startup.cs

--- src/iTechArt.Shook.Repositories/DbContexts/UserDbContext.cs
+++ src/iTechArt.Shook.Repositories/DbContexts/UserDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using iTechArt.Shook.DomainModel.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace iTechArt.Shook.Repositories.DbContexts
 {
-    public class UserDbContext : IdentityDbContext
+    public class UserDbContext : IdentityDbContext<User>
     {
         public UserDbContext(DbContextOptions<UserDbContext> options)
             : base(options)
--- src/iTechArt.Shook.WebApp/Startup.cs
+++ src/iTechArt.Shook.WebApp/Startup.cs
@@ -1,9 +1,11 @@
 using iTechArt.Common;
 using iTechArt.Shook.Repositories.DbContexts;
+using iTechArt.Shook.DomainModel.Models;
 using iTechArt.Shook.Repositories.Units;
 using iTechArt.Shook.Foundation;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,6 +32,7 @@ namespace iTechArt.Shook.WebApp
                 (Configuration.GetConnectionString("DefaultConnection")));
             services.AddSingleton<ILog, Logger>();
             services.AddControllersWithViews();
+            services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<UserDbContext>();
         }
 
         
