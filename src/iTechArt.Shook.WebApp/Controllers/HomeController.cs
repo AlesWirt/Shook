227d8503e621321989eb16bb4424ead753d0e937
Ales Wirt <sanichwi@gmail.com>
2021-12-20 17:45:19 +0300

    Delete unnecessary files

--- src/iTechArt.Shook.WebApp/Controllers/HomeController.cs
+++ src/iTechArt.Shook.WebApp/Controllers/HomeController.cs
@@ -1,24 +1,19 @@
 ﻿using iTechArt.Common;
-using iTechArt.Shook.Foundation;
-using iTechArt.Shook.DomainModel.Models;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace iTechArt.Shook.WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IClickerService _service;
         private readonly ILog _logger;
 
 
         public HomeController(ILog logger)
         {
-            _service = service;
             _logger = logger;
         }
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
             return View();
         }
